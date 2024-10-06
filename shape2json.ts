2ee662a66b10b06db2cfafb4bb231491aa10f721
大沼 峻徳 <numa.san.1024@gmail.com>
2024-10-06 19:08:36 +0900

    FILE_NAMEを変更

--- shape2json.ts
+++ shape2json.ts
@@ -6,7 +6,7 @@ import { promises as fsPromises } from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const FILE_NAME = "P17-12_13_FireStationJurisdiction";
+const FILE_NAME = "P17-12_13_FireStation";
 
 shapefile
   .read(
